61c3646f0d9c8760d59befe70c6e82b4f6e722aa
Olga P <olya-polunina@yandex.ru>
2023-11-18 20:23:37 +0300

    Task2: Replace && insted &

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -5,19 +5,19 @@ class Program
     {
         Console.Write("Введите число от -50 до 50: ");
         float userNumber = Convert.ToSingle(Console.ReadLine());
-        if (userNumber >= -40 & userNumber <= -10)
+        if (userNumber >= -40 && userNumber <= -10)
         {
             Console.WriteLine("Введенное число попало в промежуток [-40, -10]");
         }
-        else if (userNumber >= -9 & userNumber <= 0)
+        else if (userNumber >= -9 && userNumber <= 0)
         {
             Console.WriteLine("Введенное число попало в промежуток [-9, 0]");
         }
-        else if (userNumber >= 1 & userNumber <= 10)
+        else if (userNumber >= 1 && userNumber <= 10)
         {
             Console.WriteLine("Введенное число попало в промежуток [1, 10]");
         }
-        else if (userNumber >= 11 & userNumber <= 40)
+        else if (userNumber >= 11 && userNumber <= 40)
         {
             Console.WriteLine("Введенное число попало в промежуток [11, 40]");
         }
