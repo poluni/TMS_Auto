4c6a7a59ef9f9b72c426001d4bd7b8dde30f8b2c
Olga P <olya-polunina@yandex.ru>
2023-11-18 18:49:11 +0300

    Add Task 4.1 (The remainder of division is zero)

---
+++ Task4.1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task3;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите число: ");
+        long userNumber = Convert.ToInt64(Console.ReadLine());
+        bool isEven = userNumber % 2 == 0;
+        outputMessage(isEven);
+
+        void outputMessage(bool isEven)
+        {
+            if (isEven)
+            {
+                Console.Write("Число четное");
+            }
+            else
+            {
+                Console.Write("Число нечетное");
+            }
+        }
+    }
+
+}
