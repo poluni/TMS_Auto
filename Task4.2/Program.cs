778da23e312b3bef6b6a4ce158aa9b103f64c53c
Olga P <olya-polunina@yandex.ru>
2023-11-18 19:43:02 +0300

    Add Task 4_2 (bit-shift)

---
+++ Task4.2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task4_2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        {
+            Console.Write("Введите число: ");
+            long userNumber = Convert.ToInt64(Console.ReadLine());
+            bool isEven = userNumber == (userNumber >> 1) << 1;
+            outputMessage(isEven);
+
+            void outputMessage(bool isEven)
+            {
+                if (isEven)
+                {
+                    Console.Write($"Число \"{userNumber}\" - четное");
+                }
+                else
+                {
+                    Console.Write($"Число \"{userNumber}\" - нечетное");
+                }
+            }
+        }
+    }
+}
