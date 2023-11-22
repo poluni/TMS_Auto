548a152e131f97dcde2c84ae31cbc82374e88d2d
Olga P <olya-polunina@yandex.ru>
2023-11-22 21:57:56 +0300

    Add Task27

---
+++ Task2_7/Program.cs
@@ -0,0 +1,19 @@
+﻿// Тема 2 "Массивы и циклы". Задача 7.
+
+namespace TMS_Auto
+{
+    static class Task26
+    {
+        static void Main(string[] args)
+        {
+            string[] namesArray = { "Domenic", "Alex", "Paolo", "Cristian" };
+            Array.Sort(namesArray);
+            Console.WriteLine("Отсортированный массив: ");
+            foreach (string name in namesArray)
+            {
+                Console.Write($" {name} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
