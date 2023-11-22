ca39f5281bc100dc048de5d1622f082929f015fd
Olga P <olya-polunina@yandex.ru>
2023-11-22 13:28:18 +0300

    Task21: added Main method

--- Task2_1/Program.cs
+++ Task2_1/Program.cs
@@ -1,32 +1,43 @@
-﻿Console.Write("Numbers array: ");
-int[] array = new int[20];
-for (int i = 0; i <= array.GetUpperBound(0); i++)
+﻿// Тема 2 "Массивы и циклы". Задача 1
+
+namespace TMS_Auto
 {
-    array[i] = new Random().Next(100);
-    Console.Write($" {array[i]} ");
-}
-Console.WriteLine();
-
-
-Console.Write("Enter your number: ");
-int userNumber = Convert.ToInt32(Console.ReadLine());
-
-bool isInArray = false;
-foreach (int number in array)
-{
-    if (userNumber == number)
+    static class Task21
     {
-        isInArray = true;
-        break;
+        static void Main(string[] args)
+        {
+            Console.Write("Numbers array: ");
+            int[] array = new int[20];
+            for (int i = 0; i <= array.GetUpperBound(0); i++)
+            {
+                array[i] = new Random().Next(100);
+                Console.Write($" {array[i]} ");
+            }
+            Console.WriteLine();
+
+
+            Console.Write("Enter your number: ");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            bool isInArray = false;
+            foreach (int number in array)
+            {
+                if (userNumber == number)
+                {
+                    isInArray = true;
+                    break;
+                }
+            }
+
+
+            if (isInArray)
+            {
+                Console.WriteLine($"Number {userNumber} is in an array");
+            }
+            else
+            {
+                Console.WriteLine($"Number {userNumber} isn't in an array");
+            }
+        }
     }
-}
-
-
-if (isInArray)
-{
-    Console.WriteLine($"Number {userNumber} is in an array");
-}
-else
-{
-    Console.WriteLine($"Number {userNumber} isn't in an array");
 }
