c4ec91db5c320d70ce5d24dc984a92b63e226b51
Olga P <olya-polunina@yandex.ru>
2023-11-22 21:54:47 +0300

    Add Task26

---
+++ Task2_6/Program.cs
@@ -0,0 +1,42 @@
+﻿// Тема 2 "Массивы и циклы". Задача 6.
+
+namespace TMS_Auto
+{
+    static class Task26
+    {
+        static void Main(string[] args)
+        {
+
+            int[] array = new int [10];
+            for (int i = 0; i <= array.GetUpperBound(0); i++)
+            {
+                array[i] = new Random().Next(100);
+            }
+            Console.WriteLine("Изначальный массив чисел: ");
+            OutputArray(array);
+
+            int[] newArray = new int[array.GetLength(0)];
+            for (int i = 0; i <= array.GetUpperBound(0); i++)
+            {
+                if (i % 2 != 0)
+                {
+                    newArray[i] = array[i];
+                }
+            }
+            Console.WriteLine("Массив, где каждый элемент с нечётным индексом заменен на нуль: ");
+            OutputArray(newArray);
+
+            int[] OutputArray(int[] array)
+
+            {
+                for (int i = 0; i <= array.GetUpperBound(0); i++)
+                {
+                    Console.Write($" {array[i]} ");
+                }
+                Console.WriteLine();
+                return array;
+
+            }
+        }
+    }
+}
