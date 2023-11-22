1e7a91771ec6b6d51a7cc23fa1f4ce40a206134b
Olga P <olya-polunina@yandex.ru>
2023-11-22 20:38:50 +0300

    Add Task2 2

---
+++ Task2_2/Program.cs
@@ -0,0 +1,63 @@
+﻿// Тема 2 "Массивы и циклы". Задача 2.
+
+namespace TMS_Auto
+{
+    static class Task22
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[20];
+            for (int i = 0; i <= array.GetUpperBound(0); i++)
+            {
+                array[i] = new Random().Next(100);
+            }
+
+            Console.WriteLine("Изначальный массив чисел: ");
+            OutputArray(array);
+
+            Console.Write("Введите число, которое нужно удалить: ");
+            int numberToDelete = Convert.ToInt32(Console.ReadLine());
+
+            bool isInArray = false;
+            int countInArray = 0;
+            for (int i = 0; i <= array.GetUpperBound(0); i++)
+            {
+                if (numberToDelete == array[i])
+                {
+                    if (countInArray == 0) isInArray = true;
+                    countInArray++;
+                }
+            }
+
+            if (isInArray)
+            {
+                int[] newArray = new int[array.GetLength(0) - countInArray];
+                int j = 0;
+                for (int i = 0; i <= array.GetUpperBound(0); i++)
+                {
+                    if (array[i] != numberToDelete)
+                    {
+                          newArray[j] = array[i];
+                          j++;
+                    }
+                }
+                OutputArray(newArray);
+            }
+            else
+            {
+                Console.WriteLine($"Число {numberToDelete} не входит в массив");
+            }
+
+
+            int[] OutputArray(int[] array)
+            {
+                for (int i = 0; i <= array.GetUpperBound(0); i++)
+                {
+                    Console.Write($" {array[i]} ");
+                }
+                Console.WriteLine();
+                return array;
+            }
+        }
+    }
+}
