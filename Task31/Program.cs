c44f4e097f33534d1fe1c7db616415754c732d9d
Olga P <olya-polunina@yandex.ru>
2023-11-23 16:15:16 +0300

    Initialize method in Task31

---
+++ Task31/Program.cs
@@ -0,0 +1,23 @@
+﻿// Theme 3 "Strings". Task 1.
+
+using System.Reflection.PortableExecutable;
+
+namespace TMS_Auto
+{
+    static class Task31
+    {
+        static void Main(string[] args)
+        {
+            String path = @"D:\git-auto\TMS_Auto\Task31\data.txt";
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line = String.Empty; ;
+                while ((line = reader.ReadLine()) != null)
+                { 
+
+                }
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
