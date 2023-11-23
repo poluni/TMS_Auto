683a32136222a782128a68ef0172e47e2d7c10e5
Olga P <olya-polunina@yandex.ru>
2023-11-23 11:03:27 +0300

    Add Task210

---
+++ Task210/Program.cs
@@ -0,0 +1,32 @@
+﻿// Тема 2 "Массивы и циклы". Задача 10.
+
+namespace TMS_Auto
+{
+    static class Task210
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[2,3];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = new Random().Next(100);
+                    Console.Write($" {array[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+
+            int sum = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    sum += array[i, j];
+                }
+            }
+
+            Console.WriteLine($"Сумма элементов в массиве: {sum}");
+        }
+    }
+}
