acc0c3c9e1974b1420a415c60c4fe42e5d983a83
Olga P <olya-polunina@yandex.ru>
2023-11-24 12:17:20 +0300

    Add Task31 realization

--- Task31/Program.cs
+++ Task31/Program.cs
@@ -8,15 +8,23 @@ namespace TMS_Auto
     {
         static void Main(string[] args)
         {
-            String path = @"D:\git-auto\TMS_Auto\Task31\data.txt";
-            using (StreamReader reader = new StreamReader(path))
+            FileInfo fileInfo = new FileInfo(@"D:\git-auto\TMS_Auto\Task31\data.txt");
+            using (StreamReader reader = fileInfo.OpenText())
             {
-                string line = String.Empty; ;
+                string reoplacedLine = String.Empty;
+                string line = String.Empty;
                 while ((line = reader.ReadLine()) != null)
-                { 
-
+                {
+                    reoplacedLine = line.Replace("test", "testing");
+                    foreach (char symbol in reoplacedLine)
+                    {
+                        if (Char.IsDigit(symbol))
+                        {
+                            reoplacedLine = reoplacedLine.Replace(symbol, new char());
+                        }
+                    }
                 }
-                Console.WriteLine(line);
+                Console.WriteLine(reoplacedLine);
             }
         }
     }
