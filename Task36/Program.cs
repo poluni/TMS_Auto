b1406d064980c8ed152276bae50ad6031f472de1
Olga P <olya-polunina@yandex.ru>
2023-11-24 22:29:27 +0300

    Add Task36

---
+++ Task36/Program.cs
@@ -0,0 +1,22 @@
+﻿// Theme 3 "Strings". Task 6.
+
+namespace TMS_Auto
+{
+    static class Task36
+    {
+        static void Main(string[] args)
+        {
+            String path = @"D:\git-auto\TMS_Auto\Task36\input_data.txt";
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line = String.Empty;
+                string word = String.Empty;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    word = line.Trim().Replace("\t", " ").Replace("  ", " ");
+                }
+                Console.WriteLine($"Нормализованная строка: {word}");
+            }
+        }
+    }
+}
