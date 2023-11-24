300e00cb2751c15d9e494b0b2cfa2fe6bf0ea5b9
Olga P <olya-polunina@yandex.ru>
2023-11-24 23:35:10 +0300

    Task37: added string length check

---
+++ Task37/Program.cs
@@ -0,0 +1,51 @@
+﻿// Theme 3 "Strings". Task 7.
+
+namespace TMS_Auto
+{
+    static class Task37
+    {
+        static void Main(string[] args)
+        {
+            String path = @"D:\git-auto\TMS_Auto\Task37\input_data.txt";
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line = String.Empty;
+                string finalString = String.Empty;
+                string[] words = new string[] { };
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (line.Length < 100)
+                    {
+                        string normalizedString = line.Trim().Replace("\t", " ").Replace("  ", " ");
+                        words = normalizedString.Split(" ");
+                        if (words.GetLength(0) < 20)
+                        {
+                            bool isWrongWordLength = true;
+                            foreach (string word in words)
+                            {
+                                if (word.Length > 10)
+                                {
+                                    isWrongWordLength = true;
+                                    Console.WriteLine($"В строке есть слова из более чем 10 символов.");
+                                    break;
+                                }
+                                else
+                                {
+                                    isWrongWordLength = false;
+                                }
+                            }
+                            if (!isWrongWordLength)
+                            {
+                                Array.Sort(words);
+                                finalString = string.Join(" ", words);
+                                Console.WriteLine($"Отсортированная строка: {finalString}");
+                            }
+                        }
+                        else Console.WriteLine($"Число слов в строке превышает 20.");
+                    }
+                    else Console.WriteLine($"В строке больше 100 символов.");
+                }
+            }
+        }
+    }
+}
