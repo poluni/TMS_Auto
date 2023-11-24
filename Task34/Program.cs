fa5c05711ef7dbb8516b20420c7aaa4761926485
Olga P <olya-polunina@yandex.ru>
2023-11-24 22:07:36 +0300

    Add Task34

---
+++ Task34/Program.cs
@@ -0,0 +1,29 @@
+﻿// Theme 3 "Strings". Task 4.
+
+namespace TMS_Auto
+{
+    static class Task34
+    {
+        static void Main(string[] args)
+        {
+            string inputLine = "Плохой день.";
+            string removeWord = "плохой";
+            string insertWord = "Хороший";
+
+            int positionOfRemove = inputLine.Length - removeWord.Length;
+            string substringWithoutRemoveWord = inputLine.Substring(positionOfRemove);
+
+            int positionOfInsert = inputLine.IndexOf(removeWord, StringComparison.OrdinalIgnoreCase);
+            string substringWithInsertWord = substringWithoutRemoveWord.Insert(positionOfInsert, insertWord);
+
+            int positionOfPoint = substringWithInsertWord.LastIndexOf('.');
+            string substringWithInsertPunctuation = substringWithInsertWord.Insert(positionOfPoint, new String('!', 9));
+
+            int positionOfSign = substringWithInsertPunctuation.LastIndexOf('!');
+            substringWithInsertPunctuation = substringWithInsertPunctuation.Remove(positionOfSign, 1);
+            string finalString = substringWithInsertPunctuation.Insert(positionOfSign, "?");
+
+            Console.WriteLine(finalString);
+        }
+    }
+}
