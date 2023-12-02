266fd4cdf50ee0ee9e579a3e4473cf037b46782f
Olga P <olya-polunina@yandex.ru>
2023-12-02 10:22:48 +0300

    Add Task43

---
+++ Task43/ATM.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task43
+{
+    public class ATM
+    {
+        public int amount20;
+        public int amount50;
+        public int amount100;
+
+        public ATM()
+        {
+        }
+
+        public ATM(int amount20, int amount50, int amount100)
+        {
+            this.amount20 = amount20;
+            this.amount50 = amount50;
+            this.amount100 = amount100;
+        }
+
+        public void PutOnCard(int amount20 = 0, int amount50 = 0, int amount100 = 0)
+        {
+            this.amount20 += amount20;
+            this.amount50 += amount50;
+            this.amount100 += amount100;
+        }
+
+        public bool WithdrawalFromCard(int amount20 = 0, int amount50 = 0, int amount100 = 0)
+        {
+            bool isSuccess = false;
+
+            if (this.amount20 >= amount20
+                && this.amount50 >= amount50
+                && this.amount100 >= amount100)
+            {
+                if (amount20 > 0)
+                {
+                    this.amount20 += amount20;
+                    isSuccess = true;
+                    Console.WriteLine($"Выдано {amount20} купюр номиналом 20");
+                }
+                if (amount50 > 0)
+                {
+                    this.amount50 += amount50;
+                    isSuccess = true;
+                    Console.WriteLine($"Выдано {amount50} купюр номиналом 50");
+                }
+                if (amount100 > 0)
+                {
+                    this.amount100 += amount100;
+                    isSuccess = true;
+                    Console.WriteLine($"Выдано {amount100} купюр номиналом 100");
+                }
+            }
+            return isSuccess;
+        }
+    }
+}
---
+++ Task43/Program.cs
@@ -0,0 +1,32 @@
+﻿// Тема 4 "Классы и методы". Задача 3.
+
+namespace Task43
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ATM atm1 = new ATM();
+            atm1.PutOnCard(amount20: 2, amount100: 16);
+
+            Console.WriteLine(atm1.WithdrawalFromCard(amount20: 2, amount100: 16) ?
+                "Операция выполнена" : "Операция не выполнена");
+            Console.WriteLine();
+
+            Console.WriteLine(atm1.WithdrawalFromCard(amount20: 2, amount50: 16) ?
+                "Операция выполнена" : "Операция не выполнена");
+            Console.WriteLine();
+
+            Console.WriteLine(atm1.WithdrawalFromCard(amount100: 16) ?
+                "Операция выполнена" : "Операция не выполнена");
+            Console.WriteLine();
+
+            Console.WriteLine(atm1.WithdrawalFromCard(amount20: 2, amount50: 16, amount100: 16) ?
+                "Операция выполнена" : "Операция не выполнена");
+            Console.WriteLine();
+
+            Console.WriteLine(atm1.WithdrawalFromCard() ?
+                 "Операция выполнена" : "Операция не выполнена");
+        }
+    }
+}
