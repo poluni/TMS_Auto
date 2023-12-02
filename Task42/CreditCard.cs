85c5e59c1552725103edb6f2e0959469cec93f81
Olga P <olya-polunina@yandex.ru>
2023-12-02 10:21:07 +0300

    Add Task42

---
+++ Task42/CreditCard.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task42
+{
+    public class CreditCard
+    {
+        public string accountNumber;
+        public decimal balance;
+
+        public CreditCard()
+        {
+        }
+
+        public CreditCard(string accountNumber, decimal balance)
+        {
+            this.accountNumber = accountNumber;
+            this.balance = balance;
+        }
+
+        public void PutOnAccountNumber(decimal amount) => balance += amount;
+
+        public void WithdrawalFromAccountNumber(decimal amount)
+        {
+            if (balance >= amount) balance -= amount;
+            else Console.WriteLine($"На счете {accountNumber} не хватает средств");
+        }
+
+        public void ShowBalance() => Console.WriteLine(
+            string.Format("Баланс на счете {0}: {1:C2}",
+            accountNumber, balance));
+
+    }
+}
---
+++ Task42/Program.cs
@@ -0,0 +1,26 @@
+﻿// Тема 4 "Классы и методы". Задача 2.
+
+namespace Task42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreditCard creditCard1 = new CreditCard("4545454", 56894.66m);
+            CreditCard creditCard2 = new CreditCard("383783", 424373.83m);
+            CreditCard creditCard3 = new CreditCard("8383838", 35.2m);
+
+            creditCard1.ShowBalance();
+            creditCard1.PutOnAccountNumber(56.56m);
+            creditCard1.ShowBalance();
+
+            creditCard2.ShowBalance();
+            creditCard2.PutOnAccountNumber(18.00m);
+            creditCard2.ShowBalance();
+
+            creditCard3.ShowBalance();
+            creditCard3.WithdrawalFromAccountNumber(25.99m);
+            creditCard3.ShowBalance();
+        }
+    }
+}
