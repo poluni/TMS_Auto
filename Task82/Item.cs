c2a8c2c2c0e4db745d76b8771246b49ee9ad83ab
Olga P <olya-polunina@yandex.ru>
2023-12-19 22:39:12 +0300

    Added Task83

---
+++ Task82/Item.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task82
+{
+    internal class Item
+    {
+        public Guid ID { get; private set; } = Guid.NewGuid();
+
+        public string Title { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        public Item() { }
+
+        public Item(string title, decimal price, int quantity)
+        {
+            Title = title;
+            Price = price;
+            Quantity = quantity;
+        }
+    }
+}
---
+++ Task82/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task82
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<Guid, string> titleDictionary = new Dictionary<Guid, string>();
+        }
+    }
+}
