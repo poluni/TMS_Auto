f3b9e5e824285c62b7f77233da4dbce6b07e4b7e
Olga P <olya-polunina@yandex.ru>
2023-12-21 08:04:41 +0300

    Added Task82

--- Task82/Item.cs
+++ Task82/Item.cs
@@ -1,15 +1,15 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace Task82
 {
     internal class Item
     {
-        public Guid ID { get; private set; } = Guid.NewGuid();
-
         public string Title { get; set; }
 
         public decimal Price { get; set; }
@@ -23,6 +23,98 @@ namespace Task82
             Title = title;
             Price = price;
             Quantity = quantity;
+        }
+
+        public static void AddItem(ref Dictionary<string, Item>? inventory, Item? item)
+        {
+            if (item is not null)
+            {
+                inventory.Add(Guid.NewGuid().ToString(), item);
+            }
+        }
+
+        public static void DeleteItem(ref Dictionary<string, Item>? items, string id)
+        {
+            Item value;
+            if (items.Count != 0 || !string.IsNullOrEmpty(id))
+            {
+                if (items.TryGetValue(id, out value))
+                {
+                    if (value is Item it)
+                    {
+                        items.Remove(id);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Данный id отсутствует");
+                }
+            }
+        }
+
+        public static void UpdateItem(ref Dictionary<string, Item>? inventory, string? id)
+        {
+            Item value;
+            if (inventory.Count != 0 || !string.IsNullOrEmpty(id))
+            {
+                if (inventory.TryGetValue(id, out value))
+                {
+                    if (value is Item item)
+                    {
+                        Console.Write("Введите цену для обновления: ");
+                        decimal price;
+                        if (decimal.TryParse(Console.ReadLine().ToString(), out price))
+                        {
+                            item.Price = price;
+                        }
+                        Console.Write("Введите количество для обновления: ");
+                        int quantity;
+                        if (int.TryParse(Console.ReadLine().ToString(), out quantity))
+                        {
+                            item.Quantity = quantity;
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Данный id отсутствует");
+                }
+            }
+        }
+
+        public static void SearchByID(Dictionary<string, Item>? items, string? id)
+        {
+            Item value;
+            if (items.Count != 0 || !string.IsNullOrEmpty(id))
+            {
+                if (items.TryGetValue(id, out value))
+                {
+                    if (value is Item it)
+                    {
+                        Console.WriteLine($"ID {id}: {it.Title} {it.Price} {it.Quantity}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Данный id отсутствует");
+                }
+            }
+        }
+
+        public static void PrintItems(Dictionary<string, Item> dictionary)
+        {
+            foreach (var pair in dictionary)
+            {
+                Console.WriteLine($"Дата {DateTime.Now.ToString()}");
+                Console.Write($"ID: {pair.Key} ");
+                if (pair.Value is Item item)
+                {
+                    Console.Write(string.Format("Наименование: {0} ", item.Title));
+                    Console.Write(string.Format("Цена: {0:C2} ", item.Price));
+                    Console.Write(string.Format("Количество: {0}", item.Quantity));
+                    Console.WriteLine();
+                }
+            }
         }
     }
 }
--- Task82/Program.cs
+++ Task82/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace Task82
+﻿using System;
+
+namespace Task82
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Dictionary<Guid, string> titleDictionary = new Dictionary<Guid, string>();
+            //Инициализация
+            Dictionary<string, Item>? inventoryDictionary = new Dictionary<string, Item>() { };
+            Item item1 = new Item("Ручка шариковая синяя", 120.12M, 46);
+            Item item2 = new Item("Тетрадь в клеточку", 50.12M, 23);
+            Item item3 = new Item("Карандаш", 15.1M, 68);
+            Item.AddItem(ref inventoryDictionary, item1);
+            Item.AddItem(ref inventoryDictionary, item2);
+            Item.AddItem(ref inventoryDictionary, item3);
+
+            Console.WriteLine("Начальный список:");
+            Item.PrintItems(inventoryDictionary);
+
+            //Поиск по id
+            Console.Write("Введите id для поиска: ");
+            string? searchId = Console.ReadLine().ToString();
+            Console.WriteLine("Найдено:");
+            Item.SearchByID(inventoryDictionary, searchId);
+
+            //Обновление данных
+            Console.Write("Введите id обновления: ");
+            string? updateId = Console.ReadLine().ToString();
+            Item.UpdateItem(ref inventoryDictionary, updateId);
+            Item.PrintItems(inventoryDictionary);
+
+            //Удаление
+            Console.Write("Введите id для удаления: ");
+            string? deletedId = Console.ReadLine().ToString();
+            Item.DeleteItem(ref inventoryDictionary, deletedId);
+            Item.PrintItems(inventoryDictionary);
         }
     }
 }
