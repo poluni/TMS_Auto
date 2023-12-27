bbb1ed5aa6e9adb81a6be653640e20b00ac7ef55
Olga P <olya-polunina@yandex.ru>
2023-12-27 21:14:21 +0300

    Added Task91

---
+++ Task91/PointStruct.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task91
+{
+    internal struct PointStruct<T>
+    {
+        private T x;
+        private T y;
+
+        public T X { get; set; }
+        public T Y { get; set; }
+
+        public PointStruct(T x, T y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void Show() => Console.WriteLine($"({x}, {y})");
+    }
+}
---
+++ Task91/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task91
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PointStruct<int> point1 = new(2, 3);
+            PointStruct<float> point2 = new(2.2f, 3.3f);
+            PointStruct<double> point3 = new(2.2, 3.3);
+
+            point1.Show();
+            point2.Show();
+            point3.Show();
+        }
+    }
+}
