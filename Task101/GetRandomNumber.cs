bb645eb555d34c53363e6eb4b359b68fba84ee41
Olga P. <olya-polunina@yandex.ru>
2024-01-13 14:42:32 +0300

    Added Task10-1

---
+++ Task101/GetRandomNumber.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task101
+{
+    delegate int GetNumber();
+
+    internal class GetRandomNumber
+    {
+        internal static int Generate()
+        {
+            return 1;
+        }
+    }
+}
