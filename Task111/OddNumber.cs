d067c11348a6320c362807c287b94fde705d6d13
Olga P. <olya-polunina@yandex.ru>
2024-01-19 21:28:19 +0300

    Added Task 11-1

---
+++ Task111/OddNumber.cs
@@ -0,0 +1,23 @@
+﻿using LINQ;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task111;
+using Task111.helper;
+
+namespace Task111
+{
+    public class OddNumber
+    {
+        private TestIntDataSet _testIntSet = new TestIntDataSet();
+
+        public void Run()
+        {
+            var result = _testIntSet.Where(num => num % 2 == 1).Distinct();
+            Console.WriteLine("Итоговый массив: ");
+            PrintHelper.Print(result, i => Console.Write($"{i} "));           
+        }
+    }
+}
