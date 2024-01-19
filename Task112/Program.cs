51f306ff7ec69b79e825683069b552a7395b88c2
Olga P. <olya-polunina@yandex.ru>
2024-01-19 22:03:46 +0300

    Added Task 11-3

---
+++ Task112/Program.cs
@@ -0,0 +1,12 @@
+﻿using Task113;
+
+namespace Task112
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new Sorting().Run();
+        }
+    }
+}
---
+++ Task112/data/TestStringSet.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+
+namespace LINQ;
+
+public class TestStringSet
+{
+    public List<string> List { get; } = new()
+    {
+        "BYU", "TE", "AB", "ON", "SRE", "MLPQ"
+    };
+}
