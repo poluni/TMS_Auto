c963ada615309ae46feeb892b97c3771819543bf
Olga P. <olya-polunina@yandex.ru>
2024-01-26 19:23:42 +0300

    Added class work

---
+++ Task71/CustomExceptions/WrongPasswordException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task71.CustomExceptions
+{
+    internal class WrongPasswordException : Exception
+    {
+        public WrongPasswordException() { }
+
+        public WrongPasswordException(string message) : base(message)
+        { }
+    }
+}
