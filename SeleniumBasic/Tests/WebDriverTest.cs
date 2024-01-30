66b09309562712eda2cd745d29cceb7197e68107
Olga P. <olya-polunina@yandex.ru>
2024-01-30 08:06:11 +0300

    Added class work

---
+++ SeleniumBasic/Tests/WebDriverTest.cs
@@ -0,0 +1,22 @@
+using OpenQA.Selenium;
+using SeleniumBasic.Core;
+
+namespace SeleniumBasic.Tests;
+
+[TestFixture]
+public class WebDriverTest
+{
+    [Test]
+    public void FactoryDriverTest()
+    {
+        IWebDriver webDriver = new Browser().Driver;
+        IWebDriver webDriver1 = new Browser().Driver;
+        webDriver.Manage().Window.Maximize();
+        webDriver.Navigate().GoToUrl("http://onliner.by");
+        webDriver1.Manage().Window.Maximize();
+        webDriver1.Navigate().GoToUrl("http://onliner.by");
+
+        webDriver.Quit();
+        webDriver1.Quit();
+    }
+}
