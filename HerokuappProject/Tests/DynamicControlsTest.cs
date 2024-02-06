061fc1d06fc02637471d5a65bc6ef64ace4395e9
Olga P. <olya-polunina@yandex.ru>
2024-02-06 21:47:44 +0300

    Added Task14-1-Task14-4

---
+++ HerokuappProject/Tests/DynamicControlsTest.cs
@@ -0,0 +1,45 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerokuappProject.Tests;
+
+public class DynamicControlsTest : BaseTest
+{
+    [Test]
+    public void RemoveAddTest()
+    {
+        Driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/dynamic_controls");
+
+        IWebElement removeBtn = WaitsHelper.WaitForExists(By.XPath("//button[contains(text(),'Remove')]"));
+
+        IWebElement checkbox = WaitsHelper.WaitForExists(By.CssSelector("[type=checkbox]"));
+
+        removeBtn.Click();
+
+        Assert.That(WaitsHelper.WaitForVisibilityLocatedBy(By.Id("message")).Text, Is.EqualTo("It's gone!"));
+
+        Assert.That(WaitsHelper.WaitForElementInvisible(checkbox));
+    }
+
+    [Test]
+    public void EnableDisableTest()
+    {
+        Driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/dynamic_controls");
+
+        IWebElement input = WaitsHelper.WaitForExists(By.XPath("//form[@id='input-example']/descendant::input"));
+
+        Assert.That(!input.Enabled);
+
+        IWebElement enableBtn = WaitsHelper.WaitForExists(By.XPath("//button[contains(text(),'Enable')]"));
+
+        enableBtn.Click();
+
+        Assert.That(WaitsHelper.WaitForVisibilityLocatedBy(By.Id("message")).Text, Is.EqualTo("It's enabled!"));
+
+        Assert.That(input.Enabled);
+    }
+}
---
+++ HerokuappProject/Tests/FrameTest.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace HerokuappProject.Tests;
+
+public class FrameTest : BaseTest
+{
+    [Test]
+    public void FrameTextTest()
+    {
+        Driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/frames");
+
+        Driver.FindElement(By.XPath("//a[contains(text(),'iFrame')]")).Click();
+
+        Driver.SwitchTo().Frame(Driver.FindElement(By.Id("mce_0_ifr")));
+
+        var textArea = WaitsHelper.WaitForVisibilityLocatedBy(By.XPath("//body[@id='tinymce']/descendant::p")).Text;
+
+        Assert.That(textArea, Is.EqualTo("Your content goes here."));
+    }
+}
