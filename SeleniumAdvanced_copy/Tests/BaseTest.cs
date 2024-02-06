d5bac299d25f08e82e0dcff7054c081eb578c579
Olga P. <olya-polunina@yandex.ru>
2024-02-06 07:53:25 +0300

    Added Class work.

---
+++ SeleniumAdvanced_copy/Tests/BaseTest.cs
@@ -0,0 +1,28 @@
+using OpenQA.Selenium;
+using SeleniumBasic.Core;
+using SeleniumBasic.Helpers;
+using SeleniumBasic.Helpers.Configuration;
+
+namespace SeleniumBasic.Tests;
+
+//[Parallelizable(scope: ParallelScope.All)]
+//[FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
+public class BaseTest
+{
+    protected IWebDriver Driver { get; private set; }
+    protected WaitsHelper WaitsHelper { get; private set; }
+        
+    [SetUp]
+    public void FactoryDriverTest()
+    {
+        Driver = new Browser().Driver;
+        WaitsHelper = new WaitsHelper(Driver, TimeSpan.FromSeconds(Configurator.WaitsTimeout));
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        Driver.Quit();
+    }
+
+}
