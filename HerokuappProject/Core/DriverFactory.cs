e3e56455711c05ce499cea5c75f14d24df032860
Olga P. <olya-polunina@yandex.ru>
2024-02-11 00:56:13 +0300

    Added Task14-5

--- HerokuappProject/Core/DriverFactory.cs
+++ HerokuappProject/Core/DriverFactory.cs
@@ -1,6 +1,9 @@
+using AngleSharp.Io;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Interactions;
+using System.Reflection;
 using WebDriverManager;
 using WebDriverManager.DriverConfigs.Impl;
 using LogLevel = OpenQA.Selenium.LogLevel;
@@ -12,13 +15,32 @@ public class DriverFactory
     public IWebDriver? GetChromeDriver()
     {
         var chromeOptions = new ChromeOptions();
+
         chromeOptions.AddArguments("--incognito");
         chromeOptions.AddArguments("--disable-gpu");
         chromeOptions.AddArguments("--disable-extensions");
-        //chromeOptions.AddArguments("--headless");
+        //chromeOptions.AddArguments("--log-level=3");
+        //chromeOptions.AddArguments("disable-features=DownloadedBubble,DownloadedBubbleV2");
+        //chromeOptions.AddArguments("--headless");        
 
         chromeOptions.SetLoggingPreference(LogType.Browser, LogLevel.All);
         chromeOptions.SetLoggingPreference(LogType.Driver, LogLevel.All);
+
+        var pathDownload = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Resources");
+
+        Dictionary<string, object> chromePrefs = new()
+        {
+           { "downloded.default_directory", pathDownload },
+            {"savefile.default_directory", pathDownload },
+            { "download.prompt_for_download", false},
+            { "disable-popup-blocking", true},
+            { "profile.default_content_settings.images", 0 },
+            { "download.directory_upgrade", true},
+            { "safebrowsing.enabled", true},
+            { "profile.default_content_setting_values.automatic_downloads", 1}
+        };
+
+        chromeOptions.AddUserProfilePreference("prefs", chromePrefs);
 
         new DriverManager().SetUpDriver(new ChromeConfig());
         return new ChromeDriver(chromeOptions);
