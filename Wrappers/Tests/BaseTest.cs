ce1cd2f33588836afcb4408c359ed7899de7dc42
Olga P. <olya-polunina@yandex.ru>
2024-03-01 21:30:59 +0300

    Added Class Work 2

---
+++ Wrappers/Tests/BaseTest.cs
@@ -0,0 +1,61 @@
+using System.Text;
+using Allure.Net.Commons;
+using NUnit.Allure.Core;
+using OpenQA.Selenium;
+using Wrappers.Core;
+using Wrappers.Helpers;
+using Wrappers.Helpers.Configuration;
+using Wrappers.Steps;
+
+namespace Wrappers.Tests;
+
+[Parallelizable(scope: ParallelScope.All)]
+[FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
+[AllureNUnit]
+public class BaseTest
+{
+    protected IWebDriver Driver { get; private set; }
+    protected WaitsHelper WaitsHelper { get; private set; }
+
+    protected UserSteps UserSteps;
+
+    [OneTimeSetUp]
+    public static void GlobalSetup()
+    {
+        AllureLifecycle.Instance.CleanupResultDirectory();
+    }
+    
+    [SetUp]
+    public void FactoryDriverTest()
+    {
+        Driver = new Browser().Driver;
+        WaitsHelper = new WaitsHelper(Driver, TimeSpan.FromSeconds(Configurator.WaitsTimeout));
+
+        UserSteps = new UserSteps(Driver);
+        
+        Driver.Navigate().GoToUrl(Configurator.AppSettings.URL);
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        if (TestContext.CurrentContext.Result.Outcome.Status == NUnit.Framework.Interfaces.TestStatus.Failed)
+        {
+            Screenshot screenshot = ((ITakesScreenshot)Driver).GetScreenshot();
+            byte[] screenshotBytes = screenshot.AsByteArray;
+
+            //IWebElement test = Driver.FindElement(By.Id("sss"));
+            //Screenshot screenshotElement = ((ITakesScreenshot)test).GetScreenshot();
+
+            // Прикрепление скриншота к отчету
+            // Вариант 1
+            AllureLifecycle.Instance.AddAttachment("Screenshot", "image/png", screenshotBytes);
+
+            // Вариант 2
+            // AllureApi.AddAttachment("Screenshot", "image/png", screenshotBytes);
+            // AllureApi.AddAttachment("data.txt", "text/plain", Encoding.UTF8.GetBytes("This os the file content."));
+        }
+
+        Driver.Quit();
+    }
+}
---
+++ Wrappers/Tests/LoginTest.cs
@@ -0,0 +1,42 @@
+using Wrappers.Helpers.Configuration;
+using Wrappers.Pages;
+using Wrappers.Steps;
+
+namespace Wrappers.Tests;
+
+public class LoginTest : BaseTest
+{
+    [Test]
+    public void SuccessfulLoginTest()
+    {
+        LoginPage _loginPage = new LoginPage(Driver);
+        _loginPage.EmailInput.SendKeys(Configurator.AppSettings.Username);
+        _loginPage.PswInput.SendKeys(Configurator.AppSettings.Password);
+        _loginPage.ClickLoginInButton();
+
+        DashboardPage dashboardPage = new DashboardPage(Driver);
+
+        Assert.That(dashboardPage.IsPageOpened);
+    }
+
+    [Test]
+    public void SuccessfulLoginTest1()
+    {
+        UserSteps userSteps = new UserSteps(Driver);
+        DashboardPage dashboardPage = userSteps
+            .SuccessfulLogin(Configurator.AppSettings.Username, Configurator.AppSettings.Password);
+
+        Assert.That(dashboardPage.IsPageOpened);
+    }
+
+    //[Test]
+    public void InvalidUsernameLoginTest()
+    {
+        // Проверка
+        Assert.That(
+            new UserSteps(Driver)
+                .IncorrectLogin("ssdd", "")
+                .GetErrorLabelText(),
+            Is.EqualTo("Email/Login or Password is incorrect. Please try again."));
+    }
+}
